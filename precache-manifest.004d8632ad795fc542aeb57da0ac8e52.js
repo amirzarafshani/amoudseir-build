22300679e6563338801671666b8bc833cd2fbb98
amirzarafshani <amirzarafshani@gmail.com>
2020-06-08 12:32:11 +0430

    ddd

---
+++ precache-manifest.004d8632ad795fc542aeb57da0ac8e52.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef7595542cb1d899cefcabddb3a59f7f",
+    "url": "/static/media/IRANSans.ef759554.woff"
+  },
+  {
+    "revision": "e58a7175b048102719b8",
+    "url": "/static/css/main.10932f17.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "c07b284eebcc6047df1a",
+    "url": "/static/js/2.aa62a677.chunk.js"
+  },
+  {
+    "revision": "82d3718f55863e76c49ddbf22b87d77e",
+    "url": "/static/media/logo.82d3718f.png"
+  },
+  {
+    "revision": "019e45e7bbddd6a4e8cfda6d74c08291",
+    "url": "/static/media/technician.019e45e7.png"
+  },
+  {
+    "revision": "c3e2315906102c635b02db3a6666299b",
+    "url": "/static/media/IRANSans.c3e23159.eot"
+  },
+  {
+    "revision": "e58a7175b048102719b8",
+    "url": "/static/js/main.74d83524.chunk.js"
+  },
+  {
+    "revision": "2fd2464904104b41ac006f718c6de468",
+    "url": "/static/media/IRANSans.2fd24649.ttf"
+  },
+  {
+    "revision": "573ee4daba7476af837719db8e906ecd",
+    "url": "/static/media/marker-icon.573ee4da.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "c07b284eebcc6047df1a",
+    "url": "/static/css/2.de57e4de.chunk.css"
+  },
+  {
+    "revision": "7ef7ca993db2fb89ec448e4c29a87348",
+    "url": "/index.html"
+  }
+];
